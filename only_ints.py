8f8c81194f7a74c19d87abef435b847dc0dfc4d9
Oliver Hacker <oliver40@gmx.at>
2022-11-08 15:25:55 +0100

    implement only ints

---
+++ only_ints.py
@@ -0,0 +1,15 @@
+"""OnlyIntsModuel"""
+
+
+def only_ints(num1: int, num2: int) -> bool:
+    """Takes in two numbers. If they are both integers 
+    return true else false.
+
+    Args:
+        num1 (int):
+        num2 (int):
+
+    Returns:
+        bool: True if num1 and num2 are integers False otherwise
+    """
+    return (isinstance(num1, int) and isinstance(num2, int))
