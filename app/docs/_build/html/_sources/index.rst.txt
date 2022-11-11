1b28df808968d8fbbd96db078e72d92442d56066
Oliver Hacker <oliver40@gmx.at>
2022-11-11 15:23:43 +0100

    fix sphinx doc errors

---
+++ app/docs/_build/html/_sources/index.rst.txt
@@ -0,0 +1,21 @@
+.. BT_CODINGCHALLENGE documentation master file, created by
+   sphinx-quickstart on Fri Nov 11 15:12:56 2022.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to BT_CODINGCHALLENGE's documentation!
+==============================================
+
+.. toctree::
+   :maxdepth: 5
+   :caption: Contents:
+
+   modules
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
--- app/docs/conf.py
+++ app/docs/conf.py
@@ -2,17 +2,14 @@
 #
 # For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
-
 import os
 import sys
-sys.path.insert(0, os.path.abspath('..'))
-master_doc = 'index'
 
-
+sys.path.insert(0, os.path.abspath('../../'))
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'TC_Interview'
+project = 'BT_CODINGCHALLENGE'
 copyright = '2022, Oliver Hacker'
 author = 'Oliver Hacker'
 release = '0.1.0'
@@ -20,7 +17,7 @@ release = '0.1.0'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ['sphinx.ext.todo', 'sphinx.ext.viewcode', 'sphinx.ext.autodoc']
+extensions = ['sphinx.ext.autodoc']
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
